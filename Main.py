2b9fe949c16a04439d712fe40e6c7188d519c2c7
walla-42 <walla42.byu@gmail.com>
2024-06-23 15:15:04 -0600

    Updated

--- Main.py
+++ Main.py
@@ -1,8 +1,8 @@
 import Function_Dictionary as dic
 from Bio import SeqIO
+import os
 
-
-print(dic.select_chromosome('plasmodiumfalciparum_genome.fna'))
+print(dic.select_chromosome('Test_Genomes_Sequences/plasmodiumfalciparum_genome.fna'))
 
 Target=  "MASAPGLAFANITLMLDLPQLPAIFFVNVRNNFKIFMNEIKQKTVEGEDIFYPHNRINLQNKQINKMGRTRKYSNNKEWIFGNPF"
 # This target protein is a protein sequence in the above genome coding for an ATP synthase-associated protein. It has 1 exon and will work with this code, returning 'Found gene at position: 302825
@@ -14,3 +14,6 @@ fasta_file = 'selected_chromosome.fna'
 for record in SeqIO.parse(fasta_file, 'fasta'):
     print('\033[1;33m' + f"Record being analyzed: {record.description}" + '\033[0m')
     dic.search_gene(record.seq, Target)
+
+os.remove(fasta_file)
+
